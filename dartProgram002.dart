1d91a21a599274abaabab30533f27d88d662f8b5
atharva-buradkar <73945109+atharva-buradkar@users.noreply.github.com>
2023-03-02 15:18:59 +0530

    Create dartProgram002.dart

---
+++ dartProgram002.dart
@@ -0,0 +1,13 @@
+// program to print the area of a rectangle of randomized length and width
+
+import 'dart:math';
+
+
+void main() {
+  Random rng = Random();
+  
+  int length = rng.nextInt(100)+1;
+  int width = rng.nextInt(100)+1;
+  
+  print('The area of a rectangle of length $length and width $width is ${length*width} square units');
+}
