53e732b2a04561613928930e9ec7f693b40342ab
atharva-buradkar <73945109+atharva-buradkar@users.noreply.github.com>
2023-03-02 15:23:13 +0530

    Create dartProgram003.dart

---
+++ dartProgram003.dart
@@ -0,0 +1,12 @@
+// program to print the parity of a number
+
+import 'dart:math';
+
+
+void main() {
+  Random rng = Random();
+  
+  int number = rng.nextInt(100)+1;
+  
+  print('The number $number is ${number%2 == 0 ? 'even' : 'odd'}');
+}
