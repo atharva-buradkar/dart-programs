03bf5dd9073e132f443d8ad962c99473250d6f23
atharva-buradkar <73945109+atharva-buradkar@users.noreply.github.com>
2023-03-02 15:36:07 +0530

    Create dartProgram004.dart

---
+++ dartProgram004.dart
@@ -0,0 +1,30 @@
+// program to print the reverse of a string
+
+import 'dart:math';
+
+// Lowercase letters ascii value range 97-122
+// Uppercase letters ascii value range 65-90
+
+String reverseString(String str) {
+  String reverseStr = '';
+  for(int i = str.length-1; i>= 0; i--) {
+    reverseStr += str[i];
+  }
+  return reverseStr;
+}
+
+void main() {
+  Random rng = Random();
+  
+  String str = '';
+  
+  for(int i = 0; i < rng.nextInt(10)+1; i++) {
+    if(rng.nextBool()) {
+      str += String.fromCharCode(rng.nextInt(122-97)+97);
+    } else {
+      str += String.fromCharCode(rng.nextInt(90-65)+65);
+    }
+  }
+  
+  print('String: $str Reversed String: ${reverseString(str)}');
+}
